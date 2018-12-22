c01e4bf1f9415e947f6e307226d644e293fc0972
Curtis Bowden <curtis.bowden@gmail.com>
2018-12-21 23:44:29 -0800

    Finished playing with plaindromes

--- main.go
+++ main.go
@@ -1,21 +1,29 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
+	"unicode"
 )
 
 func IsPalindrome(str string) bool {
 
-  for i, j := 0, len(str); i<len(str) && i >= j;  i, j = i+1, j-1 {
-    //if( str[i] != str[j] ) { return false }
-    fmt.Printf("%c", str[i])
-    fmt.Println("This is a test!")
-  }
- 
-  return true
+	for i, j := 0, len(str)-1; i < len(str); i, j = i+1, j-1 {
+		fmt.Println("i:", i, "j:", j)
+		if unicode.ToLower(rune(str[i])) != unicode.ToLower(rune(str[j])) {
+			fmt.Println(str, "False")
+			return false
+		}
+	}
+	fmt.Println(str, "True")
+	return true
 }
 
 func main() {
-  IsPalindrome("a")
-  IsPalindrome("aba")
+
+	IsPalindrome("a")
+	IsPalindrome("aba")
+	IsPalindrome("abba")
+	IsPalindrome("Abba")
+	IsPalindrome("nbba")
+
 }
